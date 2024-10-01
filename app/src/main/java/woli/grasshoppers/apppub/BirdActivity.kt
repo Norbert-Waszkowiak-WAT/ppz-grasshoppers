6f916c3c1bd96616835bc80c2c532b90ddac5d11
kubabisi <kuba1.bisi1@gmail.com>
2024-10-01 20:57:36 +0200

    initital pipe implementation

--- app/src/main/java/woli/grasshoppers/apppub/BirdActivity.kt
+++ app/src/main/java/woli/grasshoppers/apppub/BirdActivity.kt
@@ -3,11 +3,13 @@ package woli.grasshoppers.apppub
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Timer
+import kotlin.random.Random
 
 class BirdActivity : AppCompatActivity() {
 
@@ -15,6 +17,11 @@ class BirdActivity : AppCompatActivity() {
     var tickCount = 0
 
     var birdVelocity = 0f
+
+    var pipes = mutableListOf<View>()
+    var pipeGapWidth = 300f
+
+    val random = Random(100) // todo: use time as seed
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +42,21 @@ class BirdActivity : AppCompatActivity() {
 
 
         bird.x = 50f
+
+        for (i in 1..2){
+            var newPipe = LayoutInflater.from(this).inflate(R.layout.bird_pipe, null)
+
+            val width = 100
+            val height = 1000
+
+            var params = FrameLayout.LayoutParams(width,height)
+
+            base.addView(newPipe, params)
+
+            pipes.add(newPipe)
+        }
+
+
     }
 
     override fun onBackPressed() {
@@ -93,6 +115,28 @@ class BirdActivity : AppCompatActivity() {
         }
 
         birdVelocity -= 1.5f
+
+
+        pipes[0].x -= 10
+        pipes[1].x -= 10
+
+        if (tickCount == 0){
+            pipes[0].x = resources.displayMetrics.widthPixels + 0f
+            pipes[1].x = resources.displayMetrics.widthPixels + 0f
+
+            pipes[0].rotation = 180f
+
+            pipes[0].y = random.nextInt(-800,800) + 0f
+            pipes[1].y = pipes[0].y + pipes[0].height + pipeGapWidth
+        }
+
+        if (pipes[0].x < 0){
+            pipes[0].x = resources.displayMetrics.widthPixels + 0f
+            pipes[1].x = resources.displayMetrics.widthPixels + 0f
+
+            pipes[0].y = random.nextInt(-800,800) + 0f
+            pipes[1].y = pipes[0].y + pipes[0].height + pipeGapWidth
+        }
 
 
         tickCount++
